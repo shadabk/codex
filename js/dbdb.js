cf1a933645705d3e8ab5aa944b5eba3fe6f76d20
Shayan Anwar <khansa14.comp@coep.ac.in>
2017-03-03 00:10:43 +0530

    MongoDB ready for entries
    Put the isbn and the missing info and get the book added in the mongodb

--- js/dbdb.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/chu');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-  console.log("connected");
-});
--- js/test_db.js
+++ js/test_db.js
@@ -1,58 +1,61 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/codex_test1');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-  console.log("connected");
-});
 
-var BookSchema = mongoose.Schema({
-    accountnumber : {type : Number, min : 1, required : true},
-    isbn : {type : Number, required : true},
-    title : {type : String, required : true},
-    author : {type : String, required : true},
-    publication : {type : String, required : true},
-    place_of_publication : {type : String},
-    edition : {type : Number, required : true},
-    date_of_publication : {type : Date},
-    date_of_entry : {type : Date, default : Date.now},
-    description : {type : String},
-    subject : {type : String, required : true},
-    page_count : {type : Number, min : 1},
-    language : {type : String, default : "English"},
-    shelf : {type : String, required : true}
-});
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost/codex_test1');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    // we're connected!
+    console.log("connected");
+  });
 
-// Now we will convert this schema to model
+  var BookSchema = mongoose.Schema({
+
+      accountnumber : {type : Number, required : true},
+      isbn : {type : String, required : true},
+      title : {type : String, required : true},
+      author : {type : String, required : true},
+      publication : {type : String, required : true},
+      place_of_publication : {type : String},
+      edition : {type : Number, required : true},
+      date_of_publication : {type : Date},
+      date_of_entry : {type : Date, default : Date.now},
+      description : {type : String},
+      subject : {type : String, required : true},
+      page_count : {type : String},
+      language : {type : String, default : "English"},
+      shelf : {type : String}
+  });
+
+  // Now we will convert this schema to model
 var book = mongoose.model('book', BookSchema);
 
-
-var temp = new book({
-  accountnumber : 1,
-  isbn : 123,
-  title : "Operating System and bkla blal",
-  author : "Shayan",
-  publication : "Shadab",
-  // place_of_publication : {type : String},
-  edition : 1,
-  // date_of_publication : {type : Date},
-  // date_of_entry : {type : Date, default : Date.now},
-  // description : {type : String},
-  subject : "OS",
-  // page_count : {type : Number, min : 1},
-  // language : {type : String, default : "English"},
-  shelf : "4.2DC"
-});
-
-temp.save(function (err) {
-  if (err) return console.error(err);
-  else{
-    console.log("book added");
+function add_book_to_db(ac_no, isbn, title, author, publication, publication_place, edition_book, publication_date,description, subject, page_count,language,shelf){
+    var temp = new book({
+      accountnumber :ac_no,
+      isbn : isbn,
+      title : title,
+      author : author,
+      publication : publication,
+      place_of_publication :publication_place,
+      edition : edition_book,
+      date_of_publication :publication_date,
+      // date_of_entry : Date.now,
+      description : description,
+      subject :subject,
+      page_count : page_count,
+      language : language,
+      shelf : shelf
+    });
+    temp.save(function (err) {
+      if (err) {
+        Object.keys(err.errors).forEach(function(key) {
+          var message = err.errors[key].message;
+          console.log('Validation error for "%s": %s', key, message);
+          // console.error(err);
+        });
+      }
+      else{
+        console.log("book added");
+      }
+    });
   }
-});
-
-book.find(function (err, book) {
-  if (err) return console.error(err);
-  console.log(book);
-})
